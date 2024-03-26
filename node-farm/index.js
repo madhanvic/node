456874f7cd066a0af7a8aaa08c1d5d1aee196928
Vignesh Moorthy <madhanvic@gmail.com>
2024-03-26 06:06:40 +0530

    Routing

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,28 +1,29 @@
 const fs = require("node:fs");
 const http = require("node:http");
+const url = require("node:url");
 
 /////////////////////////////////////////////
 /////////////////FILES
 
 //Blocking, Synchronous way
-// const textIn = fs.readFileSync("node-farm/txt/input.txt", "utf-8");
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
 // console.log(textIn);
 
 // const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
 
-// fs.writeFileSync("node-farm/txt/output.txt", textOut);
+// fs.writeFileSync("./txt/output.txt", textOut);
 // console.log("File written!");
 
 //Non-Blocking, Synchronous way
-// fs.readFile("node-farm/txt/start.txt", "utf-8", (err, data1) => {
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 //   console.log(data1);
-//   fs.readFile(`node-farm/txt/${data1}.txt`, "utf-8", (err, data2) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
 //     console.log(data2);
-//     fs.readFile(`node-farm/txt/append.txt`, "utf-8", (err, data3) => {
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
 //       console.log(data3);
 //       fs.writeFile(
-//         "node-farm/txt/final.txt",
+//         "./txt/final.txt",
 //         `${data2}\n${data3}`,
 //         "utf-8",
 //         (err) => {
@@ -36,9 +37,30 @@ const http = require("node:http");
 
 ///////////////////////////////////////////
 ////////////////////SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, {
+  encoding: "utf-8",
+});
+
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((request, response) => {
-  console.log(request);
-  response.end("Hello from the server");
+  const pathName = request.url;
+  if (pathName === "/" || pathName === "/overview") {
+    response.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    response.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    response.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    response.end(data);
+  } else {
+    response.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello-world",
+    });
+    response.end("Page not  found!");
+  }
 });
 
 server.listen(4200, "127.0.0.1", () => {
